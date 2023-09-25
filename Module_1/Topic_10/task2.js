347e59c0d7b63aca18bced4cc66188c65592eecb
ostersi <volodaostrovskij@gmail.com>
2023-09-26 00:00:17 +0300

    done task2

--- Module_1/Topic_10/task2.js
+++ Module_1/Topic_10/task2.js
@@ -1,1 +1,15 @@
+// Дано: масив [3, 2, "2", null, 1.5, 9.5, undefined];
+// Результат: Вивести у консоль суму чисел у масиві
 
+function calculateSumOfArray() {
+  const initialArray = [3, 2, "2", null, 1.5, 9.5, undefined];
+  let sum = 0;
+  for (let index = 0; index < initialArray.length; index++) {
+    if (typeof initialArray[index] === "number") {
+      sum += initialArray[index];
+    }
+  }
+  console.log(sum);
+}
+
+calculateSumOfArray();
