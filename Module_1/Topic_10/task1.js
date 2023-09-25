5e224f7b6ae1f305d0490167e79b73e3b5a0a882
ostersi <volodaostrovskij@gmail.com>
2023-09-25 23:59:40 +0300

    done task1

--- Module_1/Topic_10/task1.js
+++ Module_1/Topic_10/task1.js
@@ -1,1 +1,17 @@
-22
+// Дано: Функція яка приймає значення яке може бути будь-яким типом даних = "302", null, 128, 60
+
+// Результат: Вивести у консоль числа які є степенем двійки та не більші заданого значення або вивести помилку про коректний тип якщо значення не є числом
+function printPowsOf2(number) {
+  if (typeof number !== "number") {
+    console.log("incorrect type");
+    return 0;
+  }
+  for (let index = 1; index <= number; index *= 2) {
+    console.log(index);
+  }
+}
+
+printPowsOf2("302");
+printPowsOf2(null);
+printPowsOf2(128);
+printPowsOf2(60);
