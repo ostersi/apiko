ff7e1c135d74243b862e32563507f4e0b1bdec21
ostersi <volodaostrovskij@gmail.com>
2023-09-26 13:26:25 +0300

    done task4

--- Module_1/Topic_10/task4.js
+++ Module_1/Topic_10/task4.js
@@ -1,6 +1,20 @@
-let str = "123";
-console.log(typeof str); // string
-
-let num = Array[str]; // стає числом 123
-
-console.log(typeof num); // number
+function printWordCount(string) {
+   let count = 0;
+   let isDelimited = true;
+ 
+   for (let char of string) {
+     if (char === " ") {
+       isDelimited = true;
+     } else if (isDelimited) {
+       count++;
+       isDelimited = false;
+     }
+   }
+ 
+   console.log(count);
+ }
+ 
+ printWordCount("Easy string for count");
+ printWordCount("Easy");
+ printWordCount("Some string with a triple   space");
+ printWordCount("Some?  string, with a triple   space");
