350a1356868a37a79f5ece145551c0a196625884
ostersi <volodaostrovskij@gmail.com>
2023-09-26 13:28:08 +0300

    done task4

--- Module_1/Topic_10/task4.js
+++ Module_1/Topic_10/task4.js
@@ -1,20 +1,20 @@
 function printWordCount(string) {
-   let count = 0;
-   let isDelimited = true;
- 
-   for (let char of string) {
-     if (char === " ") {
-       isDelimited = true;
-     } else if (isDelimited) {
-       count++;
-       isDelimited = false;
-     }
-   }
- 
-   console.log(count);
- }
- 
- printWordCount("Easy string for count");
- printWordCount("Easy");
- printWordCount("Some string with a triple   space");
- printWordCount("Some?  string, with a triple   space");
+  let count = 0;
+  let space = true;
+
+  for (let char of string) {
+    if (char === " ") {
+      space = true;
+    } else if (space) {
+      count++;
+      space = false;
+    }
+  }
+
+  console.log(count);
+}
+
+printWordCount("Easy string for count");
+printWordCount("Easy");
+printWordCount("Some string with a triple   space");
+printWordCount("Some?  string, with a triple   space");
