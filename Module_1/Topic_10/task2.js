2b844abbde2f45062c69a182ec5f9ed2f9e08377
ostersi <volodaostrovskij@gmail.com>
2023-10-03 10:02:12 +0300

    some change in index.html and task2.js

--- Module_1/Topic_10/task2.js
+++ Module_1/Topic_10/task2.js
@@ -1,8 +1,8 @@
 // Дано: масив [3, 2, "2", null, 1.5, 9.5, undefined];
 // Результат: Вивести у консоль суму чисел у масиві
-
+const initialArray = [3, 2, "2", null, 1.5, 9.5, undefined];
 function calculateSumOfArray() {
-  const initialArray = [3, 2, "2", null, 1.5, 9.5, undefined];
+  
   let sum = 0;
   for (let index = 0; index < initialArray.length; index++) {
     if (typeof initialArray[index] === "number") {
