c540397e3eb2eb0297ba0fa304372657731a40c4
ostersi <volodaostrovskij@gmail.com>
2023-10-14 19:36:16 +0300

    add task3

--- Module_1/Topic_18/task3.js
+++ Module_1/Topic_18/task3.js
@@ -1,18 +1,21 @@
 // Дано: код в якому необхідно знайти помилку і виправити її
 // Результат: вивести в консоль “meow”
-// const cat = {
-//   sound: "meow",
-//   greet: function () {
-//     setTimeout(
-//       function () {
-//         console.log(this.sound);
-//       }, // write fix in this line of code
-//       0
-//     );
-//   },
-// };
-// cat.greet(); // should produce "meow"
 
+//До:
+const cat2 = {
+  sound: "meow",
+  greet: function () {
+    setTimeout(
+      function () {
+        console.log(this.sound);
+      }, // write fix in this line of code
+      0
+    );
+  },
+};
+cat2.greet(); // should produce "meow"
+
+//Після:
 const cat = {
   sound: "meow",
   greet: function () {
